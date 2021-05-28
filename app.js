11e2961a2a95eb4da5b11f8eb5c4243dc2fee98b
Jason Chan <jchan321738@gmail.com>
2021-05-28 09:25:59 -0700

    complete

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-let answer = ``;
-let guessed = [];
+let word = ``;
 let wordGuess = null;
+let guessed = [];
+let maxWrongLetter = 6;
+let wrongLetter = 0;
+let audioWin = new Audio('./assets/audio/win.mp3');
+let audioLost = new Audio('./assets/audio/lost.mp3');
+
 
 // Choose country
 let words = ["australia", "belize", "canada", "switzerland", "mexico", "nicaragua"];
 
 function chooseWord() {
-    answer = words[Math.floor(Math.random() * words.length)];
+    word = words[Math.floor(Math.random() * words.length)];
 }
 chooseWord();
 
@@ -14,8 +19,8 @@ chooseWord();
 function alphabetButtons() {
     let buttons = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => 
         `
-        <button class="btn btn-danger m-2" id="` + letter + `" onClick="handleGuess('` + letter + `')">
-            ` + letter + `
+        <button class="btn btn-danger m-2" id="${letter}" onClick="handleGuess('${letter}')">
+            ${letter}
         </button>
         `).join('')
     document.getElementById('alphabet').innerHTML = buttons
@@ -24,7 +29,7 @@ alphabetButtons();
 
 // Guess Word
 function guessWord() {
-    wordGuess = answer.split('').map(letter => 
+    wordGuess = word.split('').map(letter => 
         (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
 
     document.getElementById('wordDisplay').innerHTML = wordGuess
@@ -36,11 +41,47 @@ function handleGuess(letter) {
     guessed.indexOf(letter) === -1 ? guessed.push(letter) : null;
     document.getElementById(letter).setAttribute('disabled', true);
 
-    if (answer.indexOf(letter) >= 0) {
+    if (word.indexOf(letter) >= 0) { // if true
         guessWord();
+        checkWon();
+    } else if (word.indexOf(letter) === -1) { // if false
+        wrongLetter++;
+        checkLost();
+        updateImg()
     }
 }
 handleGuess();
 
+// if won
+function checkWon() {
+    if (wordGuess === word) {
+        audioWin.play()
+        alert("You Won!")
+    }
+}
+// if lost
+function checkLost() {
+    if (wrongLetter === maxWrongLetter) {
+        audioLost.play()
+        document.getElementById('wordDisplay').innerHTML = "The answer was: " + word
+        alert("You Lost!")
+    }
+}
+
+// update Image
+function updateImg() {
+    document.getElementById('hangman-img').src = './assets/images/hangman_' + wrongLetter + '.png'
+}
+
+// reset button
+function reset() {
+    wrongLetter = 0;
+    guessed = [];
+    document.getElementById('hangman-img').src = './assets/images/hangman_0.png'
+
+    chooseWord();
+    alphabetButtons();
+    guessWord();
+}
 
 
